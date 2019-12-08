24369f807cda700551f672d7950db7c78387d893
Jack- laptop <c1633583@mydit.ie>
2019-12-08 19:10:16 +0000

    some audioVis

---
+++ GE-CA/Assets/Resources/getAudioData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class getAudioData : MonoBehaviour
+{
+    // Listen to the audio source every frame and get samples from spectrum data into the float array
+    AudioSource a_audioSource;
+    //
+    public static float[] fa_samples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a_audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        a_audioSource.GetSpectrumData(fa_samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ GE-CA/Assets/Resources/spawnCubes.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnCubes : MonoBehaviour
+{
+
+    public int numObjects = 50;
+    public GameObject Cube;
+    public GameObject[,] ring = new GameObject[300, 300];
+   
+    public int radius = 10;
+    public float maxScale = 50;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get position of camera
+        Vector3 centerPos = Camera.main.transform.position;
+        centerPos.z += 5;
+       for (int j = 0; j < 40; j++)
+       {
+            centerPos.z += 1;
+            for (int i = 0; i < numObjects; i++)
+            {
+                Vector3 vec = Quaternion.Euler(0, -90, 0) * Vector3.right;
+                Quaternion rot = Quaternion.AngleAxis(i * 8, vec);
+                Vector3 direction = rot * Vector3.up;
+
+                Vector3 position = centerPos + (direction * radius);
+                GameObject instanceCube = (GameObject)Instantiate(Cube, position, rot);
+
+                ring[j,i] = instanceCube;
+                //tunnel[j] = ring[i];
+
+            }
+            
+        }
+        Debug.Log(ring.Length);
+        Debug.Log(getAudioData.fa_samples.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int l = 0; l < 40; l++)
+        {
+            for (int k = 0; k < numObjects; k++)
+            {
+                if (ring != null)
+                {
+                    ring[l,k].transform.localScale = new Vector3(1, (getAudioData.fa_samples[k] * maxScale) + 2, 1);
+                }
+            }
+        }
+    }
+}
