00d0392f92fc9fbf319fab7b98a5cbf94ce7d33f
Jack- laptop <c1633583@mydit.ie>
2019-12-08 22:26:36 +0000

    some camera movement

---
+++ GE-CA/Assets/Resources/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform cameraTarget;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.z += 5;
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 2.0f);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+        cameraPos.z += 10;
+        Gizmos.DrawSphere(cameraPos,  1);
+    }
+}
--- GE-CA/Assets/Resources/cubescript.cs
+++ GE-CA/Assets/Resources/cubescript.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class cubescript : MonoBehaviour
 {
-    
-    Renderer m_Renderer;
+   
     // Start is called before the first frame update
     void Start()
     {
-        m_Renderer = GetComponent<Renderer>();
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!m_Renderer.isVisible)
-        {
-            Destroy(gameObject);
-        }
+       
     }
 }
